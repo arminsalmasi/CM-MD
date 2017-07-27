58bd1de138fa25fb581f981ea8ce53d4bce97cef
hana <saharasadi@gmail.com>
2017-07-27 15:01:25 +0200

    Commit#15 code refactor in md_md module
    methods mentioned commit #14 are all subroutines in md_md: do_rand_xyz, do_rand_vel, do_fix_com, do_scale_vel
    
    ToDo: 1- implement velocity update
    ToDo: 2- initialize acceleration vector!

--- fortran/md_md.f90
+++ fortran/md_md.f90
@@ -12,7 +12,7 @@ contains
     ! allocate tmstp datatype vector
     ! allocate all vector fileds of all cells of tmstp vector
     
-      type(tmstp_holder), dimension(:), allocatable :: tmstp
+      type(tmstp_holder), allocatable :: tmstp(:)
       allocate(tmstp(0:floor(t/dt)))                           
      
     ! loops over all timesteps to allocate variables in 
@@ -28,9 +28,7 @@ contains
    !##########################################################
    
    subroutine do_loop_tstps(tmstp)
-   
   
- 
    ! loops over all timesteps(N_tstp=t/dt)
    !   in tstp=0:
    !   in 0<tstp<N_tstp
@@ -44,55 +42,21 @@ contains
        IF (i == 0) then
                          
          ! randomize xyz of atoms in cell 0 
-         DO j = 1, N_atms
-          
-           call do_genRand(r,3,j)
-           tmstp(i)%xyz(j,:) = r(:) * ( box_vol**(1.0/3.0))
-         
-         END DO
-                      
+                               !print *, tmstp(i)%xyz    
+         CALL do_rand_xyz(tmstp(i)%xyz)
+             
          ! randomize velocities of atoms in cell zero
-         DO j = 1 , N_atms
-           r(:) = rand_normal2(3) ! open source from roseta code
-           vstd = sqrt( tmp * kb / atm_masses(j) )
-           tmstp(i)%vel(j,:) = 0 + r(:) * vstd  
-         END Do  
+         CALL do_rand_vel(tmstp(i)%vel)          
                                !CALL do_calcT(tempT, tmstp(i)%vel)
                                !PRINT *, 'tempT= ', tempT,'T= ', tmp
          
          ! fix center of mass
                                !print *,tmstp(i)%vel(1,:)
-         DO j = 1, n_atms
-           vcm_tmp(j,:) = tmstp(i)%vel(j,:) * atm_masses(j)
-         END DO
-         vcm = sum(vcm_tmp) / sum(atm_masses)
-                               !print *,vcm
-         DO j = 1, n_atms
-           tmstp(i)%vel(j,:) = tmstp(i)%vel(j,:) - vcm
-         END DO
-                              !print *,tmstp(i)%vel(1,:)
+         CALL do_fix_com(tmstp(i)%vel)
+                               !print *,tmstp(i)%vel(1,:)
         
          ! Scale velocities with initial temperature
-           k = 1 
-           CALL do_calcT(tempT, tmstp(i)%vel)
-           tmp_scale = ( (tmp - tempT) / tmp )  * 100
-                               !PRINT *, 'tempT = k:',k ,'=', tempT, "T = ", tmp, "tmp_scale= ", tmp_scale
-           DO WHILE ( (ABS(tmp_scale)>0.001) .and. (k<1000) )
-              
-              vel_scale = sqrt(tmp / tempT)
-                               !Print *, 'vel_scale', k, '=', vel_scale
-              DO j = 1 , n_atms
-                tmstp(i)%vel(j,:) = tmstp(i)%vel(j,:) * vel_scale
-              END DO 
-           
-              CALL do_calcT(tempT, tmstp(i)%vel)
-              tmp_scale = ( (tmp - tempT) / tmp )  * 100
-              
-                               !PRINT *, 'tempT = k:',k ,'=', tempT, "T = ", tmp, "tmp_scale= ", tmp_scale
-              k= k + 1  
-             
-           end DO
-                           
+           CALL do_scale_vel(tmstp(i)%vel)
        ELSE
          ! TODO : do md on each timestep
          ! do_md()
@@ -104,6 +68,86 @@ contains
    END SUBROUTINE do_loop_tstps
    
    !##########################################################
+   
+   SUBROUTINE do_rand_xyz(xyz)
+   
+     INTEGER :: j
+     REAL(dp) :: r(3), xyz(:,:)
+     
+     DO j = 1, N_atms
+    
+       call do_genRand(r,3,j)
+       xyz(j,:) = r(:) * ( box_vol**(1.0/3.0))
+   
+     END DO
+   
+   END SUBROUTINE do_rand_xyz
+   
+   !##########################################################
+   
+   SUBROUTINE do_rand_vel(vel)
+     
+     IMPLICIT NONE
+     
+     REAL(dp)  :: vel(:,:) , r(3), vstd 
+     INTEGER :: j
+     
+     DO j = 1 , N_atms
+       
+       r(:) = rand_normal2(3) ! open source from roseta code
+       vstd = sqrt( tmp * kb / atm_masses(j) )
+       vel(j,:) = 0 + r(:) * vstd  
+     
+     END Do
+   
+   END SUBROUTINE do_rand_vel
+   
+   !##########################################################
+   SUBROUTINE do_fix_com(vel)
+     
+     IMPLICIT NONE
 
+     INTEGER :: j
+     REAL(dp) :: vel(:,:), vcm_tmp(SIZE(vel,1),SIZE(vel,2)), vcm 
+
+
+     DO j = 1, n_atms
+       vcm_tmp(j,:) = vel(j,:) * atm_masses(j)
+     END DO
+     vcm = SUM(vcm_tmp) / SUM(atm_masses)
+                           !print *,vcm
+     DO j = 1, n_atms
+       vel(j,:) = vel(j,:) - vcm
+     END DO
+   END SUBROUTINE do_fix_com
+   
+   !##########################################################
+   SUBROUTINE do_scale_vel(vel)
+   
+     IMPLICIT NONE
+     
+     INTEGER :: k, j
+     real(dp) :: vel(:,:), trmpT, tmp_scale, vel_scale, tempT
+     
+       k = 1 
+       CALL do_calcT(tempT, vel)
+       tmp_scale = ( (tmp - tempT) / tmp )  * 100
+                           !PRINT *, 'tempT = k:',k ,'=', tempT, "T = ", tmp, "tmp_scale= ", tmp_scale
+       DO WHILE ( (ABS(tmp_scale)>0.001) .and. (k<1000) )
+          vel_scale = sqrt(tmp / tempT)
+                           !Print *, 'vel_scale', k, '=', vel_scale
+          DO j = 1 , n_atms
+            vel(j,:) = vel(j,:) * vel_scale
+          END DO 
+          CALL do_calcT(tempT, vel)
+          tmp_scale = ( (tmp - tempT) / tmp )  * 100
+                            !PRINT *, 'tempT = k:',k ,'=', tempT, "T = ", tmp, "tmp_scale= ", tmp_scale
+          k= k + 1  
+       end DO
+
+   END SUBROUTINE do_scale_vel                   
+
+   !##########################################################
+    
 end module md
 
